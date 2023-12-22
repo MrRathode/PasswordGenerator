a5521631dbcf0a5a36ae3cdf990cc38ba9a2cb98
Naveen Kumar <112309436+MrRathode@users.noreply.github.com>
2023-12-22 15:48:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
-const password_element = document.getElementById("password_text");
-var string = "ABCDEFGHIJKLMNOPQRSTUVWXYZacdefghijklnopqrstuvwxyz0123456789";
-const special_chars = "!(){}[]@#$%^&*";
-const generate = document.getElementById("generate");
-const clipboard = document.getElementById("clipboard");
-var pwd_length = document.getElementById("slider");
+const passwordElement = document.getElementById("password_text");
+const generateButton = document.getElementById("generate");
+const clipboardButton = document.getElementById("clipboard");
+const passwordLengthInput = document.getElementById("slider");
 
+generateButton.addEventListener('click', generatePassword);
+clipboardButton.addEventListener('click', copyToClipboard);
 
-generate.addEventListener('click', () => {
+function generatePassword() {
     let password = "";
-    var checked = document.getElementById("checkbox").checked;
-    var final_string = string;
-    console.log(checked);
-    if (checked) {
-        final_string +="!(){}[]@#$%^&*";
+    const string = "ABCDEFGHIJKLMNOPQRSTUVWXYZacdefghijklnopqrstuvwxyz0123456789";
+    const specialChars = "!(){}[]@#$%^&*";
+    let finalString = string;
+
+    if (document.getElementById("checkbox").checked) {
+        finalString += specialChars;
     }
-    for (var i = 0; i < pwd_length.value; i++) {
-        let pwd = final_string[Math.floor(Math.random() * final_string.length)];
+
+    for (let i = 0; i < passwordLengthInput.value; i++) {
+        let pwd = finalString[Math.floor(Math.random() * finalString.length)];
         password += pwd;
     }
-    password_element.innerText = password;
-    final_string = string;
-});
 
+    passwordElement.innerText = password;
+}
 
-clipboard.addEventListener('click', () => {
-    navigator.clipboard.writeText(password_element.innerText);
+function copyToClipboard() {
+    navigator.clipboard.writeText(passwordElement.innerText);
     alert("Copied to your clipboard");
-});
+}
